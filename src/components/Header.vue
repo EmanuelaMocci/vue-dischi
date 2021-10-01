8ec1484533561c98ec657404cc4d391dfcd61ef6
EmanuelaMocci <emanuelamocci2@gmail.com>
2021-10-01 17:42:50 +0200

    Aggiunta header

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="header">
+       <img src="../assets/img/spotify-logo.png" alt="">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style lang="scss" scoped>
+.header{
+    background-color: #2E3A46;
+
+    img{
+        width: 100px;
+        padding: 10px 20px;
+    }
+}
+</style>
